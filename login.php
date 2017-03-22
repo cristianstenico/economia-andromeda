c84aed26f7dec9018b0f152c23d4af84402eed52
cristianstenico <cristianstenico@gmail.com>
2017-03-22 15:08:58 +0100

    use php session

---
+++ login.php
@@ -0,0 +1,14 @@
+<html> 
+<head> 
+<title>Festival dell'Economia 2016</title> 
+</head>
+<body>
+<form method="post">
+Login:<br/>
+<input type="password" size="40" name="pass" />
+<br /><br />
+<br /><br />
+<input type="submit" value="Login" />
+</form>
+</body>
+</html>
